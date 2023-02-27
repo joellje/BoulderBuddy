2191ae68673034c75b02e1bf6bdb1fc0549ea8f9
Joel Lim <joellimjieen@hotmail.com>
2023-02-26 23:22:31 -0800

    routeModel v1

---
+++ backend/models/routeModel.js
@@ -0,0 +1,44 @@
+const mongoose = require("mongoose");
+
+const routeSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Enter a name."],
+  },
+  location: {
+    type: String,
+    default: "Unknown",
+  },
+  grade: {
+    type: String,
+    default: "Unknown",
+  },
+  status: {
+    type: String,
+    enum: ["Completed", "Flashed", "Attempted", "Projected", "Unmarked"],
+    default: "Unmarked",
+  },
+  type: {
+    type: String,
+    enum: ["Slab", "Overhang", "Vertical", "Unmarked"],
+    default: "Unmarked",
+  },
+  imageURL: {
+    type: String,
+    default: "",
+  },
+  videoURL: {
+    type: String,
+  },
+  userId: {
+    type: String,
+    default: "",
+  },
+  lastUpdatedDate: {
+    type: Date,
+  },
+});
+
+const routeModel = mongoose.model("Route", routeSchema);
+
+module.exports = routeModel;
