6cbf9b1b1d6b7e7388e1758df92d60e95e5c9f47
Joel Lim <joellimjieen@hotmail.com>
2023-02-26 22:17:47 -0800

    setup connection to MongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,24 @@
 const app = require("./app.js");
+const mongoose = require("mongoose");
 require("dotenv").config();
+
+const connectionString = process.env.ATLAS_URI.replace(
+  "<PASSWORD>",
+  process.env.ATLAS_PASSWORD
+);
+
+mongoose.set("strictQuery", true);
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Database Connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const port = process.env.port || 5000;
 app.listen(port, () => {
